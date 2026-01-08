5cb34d40e5eb952244a5b0f7cb073f28b56142b3
MVSSKRISHNA <mvssskrishna@gmail.com>
2026-01-08 19:05:25 +0530

    Update git_test.py

--- test/git_test.py
+++ test/git_test.py
@@ -1,8 +1,11 @@
 class test_class():
     def test_func(spark,args_list):
         print('This is a test script')
-        test_arg = args_list[args_list.index("--test_arg") +1]
+        test_arg1 = args_list[0]
+        test_arg2 = args_list[1]
+        test_arg3 = args_list[2]
         print('Printing test argument : ',test_arg)
-        
+        print('\n',test_arg2)
+        print('\n',test_arg3)
 if __name__ == '__main__':
     test_class().test_func()
