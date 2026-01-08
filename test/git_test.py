c28a7413a1c13bbfbf26c1919545a1e4a12fec2d
MVSSKRISHNA <mvssskrishna@gmail.com>
2026-01-08 17:10:40 +0530

    Create git_test.py

---
+++ test/git_test.py
@@ -0,0 +1,12 @@
+import boto3
+import os
+import pandas as pd
+ 
+class test_class():
+    def test_func(spark,args_list):
+        print('This is a test script')
+        test_arg = args_list[args_list.index("--test_arg") +1]
+        print('Printing test argument : ',test_arg)
+        
+if __name__ == '__main__':
+    test_class().test_func()
