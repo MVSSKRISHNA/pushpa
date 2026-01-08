dcb555a08076fe1d1269b75a3f427da42457e48c
MVSSKRISHNA <mvssskrishna@gmail.com>
2026-01-08 19:09:18 +0530

    Update git_test.py

--- test/git_test.py
+++ test/git_test.py
@@ -4,7 +4,7 @@ class test_class():
         test_arg1 = args_list[0]
         test_arg2 = args_list[1]
         test_arg3 = args_list[2]
-        print('Printing test argument : ',test_arg)
+        print('Printing test argument : ',test_arg1)
         print('\n',test_arg2)
         print('\n',test_arg3)
 if __name__ == '__main__':
